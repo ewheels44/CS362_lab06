3a695442357ec7ee218b93273a6314081c7ab397
Ethan Wheeler <ethanwheeler@10-107-208-228.vlan895.client.wireless.uic.edu>
2022-03-28 14:17:26 -0500

    UPDATE: working lab with serial communication between 2 arduinos

--- ewheel5lab6.ino
+++ ewheel5lab6.ino
@@ -1,14 +1,32 @@
+/*
+   Name: Khizer Shareef - NetID: kshare2 , Ethan Wheeler - NetID: ewheel5
+   Class: CS362 SP22
+   Lab: 06 - Serial Communication ( option 1 )
 
-char recieved[1]; //Initialized variable to store recieved data
 
-const int button_pin = 11;   // button led on and off
+
+   Description: Transmission over Serial connection between two arduinos
+
+
+
+   Assumptions: Successfully implemented the knowledge of Serial connection. Used the 0 (RX) pin and 1 (TX) pin to send and receive data. In the beginning we connected 
+                both boards on a one-to-one connection with pins 0,1 which obviously was not working. Then using simple logic we connected both boards vice versa on pins 0,1 that
+                way one board sent a transimssion buf and the other read it. The hint for using the common ground was very helpful.
+
+   In-person TA: Michael
+
+*/
+const int button_pin = 12;   // button led on and off
+const int yellow =  10;      // the number of the LED pin
 
 int buttonState = 0;         // variable for reading the pushbutton status
 
-const int yellow =  10;      // the number of the LED pin
+int buttonState_prev = 0;         // prev button state for button switch
+
+int on_off_toggle = 0;          // 0 = off 1 = on 
 
 void setup() {
-      
+
   // setting up serial
   Serial.begin(9600);
   pinMode(yellow, OUTPUT);
@@ -16,31 +34,38 @@ void setup() {
 }
 
 void loop() {
-  buttonState = digitalRead(button_pin);    // pressed = 0, not pressed = 1
 
-  Serial.println(buttonState);
+   if(Serial.available() > 0){              // check if there is msg to be recived 
+      int recived = Serial.read();          // read msg
 
-  if (Serial.available()) {
-    Serial.readBytes(recieved,1); //Read the serial data and store in var
-    Serial.println(recieved); //Print data on Serial Monitor
+      //
+      // turn light on or off 0 = OFF, 1 = ON
+      //
+      if (recived == 0) { 
+        digitalWrite(yellow, LOW); 
+      }
+      if (recived == 1) { 
+        digitalWrite(yellow, HIGH); 
+      }
 
-    digitalWrite(yellow, HIGH);
+   }
 
+  buttonState = digitalRead( button_pin );
+
+  if(buttonState != buttonState_prev){          // if button state doesnt equal then turn on or off
+
+    
+    if( buttonState == HIGH){                   // when pressed
+      if (on_off_toggle == 0){                  // if on_off_toggle = 0, turn light on
+        Serial.write(1);
+        on_off_toggle = 1;                      // set light state to on
+      }
+      else{
+        Serial.write(0);                        // else the light was on, and turn it off
+        on_off_toggle = 0;
+      }
+    }
   }
-  else{
-    digitalWrite(yellow, LOW);
-  }
 
-  if(buttonState == 0){
-    char thismsg[1] = "0";
-    Serial.write(thismsg,5); //Write the serial data
-
-  }
-
-  /* else{ */
-
-  /*   char thismsg[1] = "0"; */
-  /*   Serial.write(thismsg,5); //Write the serial data */
-  /*   delay(2000); */
-  /* } */
+  buttonState_prev = buttonState;             // set curent buttonState to prev buttonState
 }
